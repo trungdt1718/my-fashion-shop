8e1059542367d27cd48c8b8d1eaf0007287dc8af
trungdt1718 <trungdt1718@gmail.com>
2025-04-13 16:10:23 +0700

    Add cicd.yml file

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -14,8 +14,8 @@ describe('App Component', () => {
     render(<App />);
     const viteLogo = screen.getByAltText(/vite logo/i);
     const reactLogo = screen.getByAltText(/react logo/i);
-    
+
     expect(viteLogo).toBeInTheDocument();
     expect(reactLogo).toBeInTheDocument();
   });
-}); 
+});
